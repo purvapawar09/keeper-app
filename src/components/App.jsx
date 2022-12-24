a7c9561f7d1dd861e578aa59be4122b1de95a358
purvapawar06 <purva.p2002@gmail.com>
2022-12-24 13:28:09 -0600

    added noted through mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
+function createNote(oneNote){
+    return <Note 
+        key={oneNote.key}
+        title={oneNote.title}
+        content={oneNote.content}
+    />
+}
 function App(){
     return <div>
         <Header/>
-        <Note/>
-        <Note/>
+        {notes.map(createNote)}
         <Footer/>
     </div>
 }
