f8f740d58256550c0d861cee3d0ad102611695a1
purvapawar06 <purva.p2002@gmail.com>
2022-12-26 20:17:04 -0600

    add and delete note functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Note(props){
-    return <div className="note">
+function Note(props) {
+    function handleDelete(){
+        props.onDelete(props.id);
+    }
+    return (
+      <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-    </div>
-}
+        <button onClick= {handleDelete}>DELETE</button>
+      </div>
+    );
+  }
 
 export default Note;
